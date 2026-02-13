76ecb381fc8383f3265437d9c63107eef0228eee
Jean <jeanpi.22241@gmail.com>
2026-02-12 20:10:09 -0500

    Separo router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import AppRouter from "./presentation/router/app.router"
 import AppProviders from "./presentation/composition/providers/app.providers"
 
 function App() {
   return (
     <AppProviders>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<AppRouter />} />
-        </Routes>
-      </BrowserRouter>
+      <AppRouter/>
     </AppProviders>
   )
 }
--- src/presentation/router/app.router.tsx
+++ src/presentation/router/app.router.tsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeSmartComponent from "../pages/modules/home/home-smart/home-smart.component";
 
 export default function AppRouter() {
   return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeSmartComponent/>}/>
       </Routes>
+    </BrowserRouter>
   )
 }
